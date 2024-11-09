1bda8de6e17cc952c18ed6e8d8044e2bc0d2934d
Bajindul <ukisaw296@gmail.com>
2024-11-10 04:11:34 +0700

    Menambah fitur crud pada halaman siswa

--- lib/pages/Siswa/siswaAddPage.dart
+++ lib/pages/Siswa/siswaAddPage.dart
@@ -1,35 +1,246 @@
+import 'package:data_siswa/model/dataSiswa.dart';
+import 'package:data_siswa/repository/dataSiswa.dart';
+import 'package:data_siswa/widgets/appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:intl/intl.dart';
 
-class SiswaPage extends StatefulWidget {
-  const SiswaPage({super.key});
+class SiswaAddPage extends StatefulWidget {
+  final DataSiswa? dataSiswa;
+  const SiswaAddPage({super.key, this.dataSiswa});
 
   @override
-  State<SiswaPage> createState() => _SiswaPageState();
+  _SiswaAddPageState createState() => _SiswaAddPageState();
 }
 
-class _SiswaPageState extends State<SiswaPage> {
+class _SiswaAddPageState extends State<SiswaAddPage> {
+  final _formKey = GlobalKey<FormState>();
+  final DataSiswaRepository repository = DataSiswaRepository();
+
+  late TextEditingController _nisController;
+  late TextEditingController _namaController;
+  late TextEditingController _tanggalLahirController;
+  late TextEditingController _alamatController;
+  List<DataKota> _selectedKota = [];
+  String? jenkel;
+
+  @override
+  void initState() {
+    super.initState();
+    _nisController = TextEditingController(text: widget.dataSiswa?.nis ?? '');
+    _namaController = TextEditingController(text: widget.dataSiswa?.nama ?? '');
+    _tanggalLahirController =
+        TextEditingController(text: widget.dataSiswa?.tanggal_lahir ?? '');
+    _alamatController =
+        TextEditingController(text: widget.dataSiswa?.alamat ?? '');
+    _selectedKota = widget.dataSiswa?.kota.toList() ?? [];
+    jenkel = widget.dataSiswa?.jenkel;
+  }
+
+  @override
+  void dispose() {
+    _nisController.dispose();
+    _namaController.dispose();
+    _tanggalLahirController.dispose();
+    _alamatController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _saveSiswa() async {
+    if (_formKey.currentState?.validate() ?? false) {
+      final siswa = DataSiswa(
+        id: widget.dataSiswa?.id ?? UniqueKey().toString(),
+        nis: _nisController.text,
+        nama: _namaController.text,
+        tanggal_lahir: _tanggalLahirController.text,
+        jenkel: jenkel ?? '',
+        alamat: _alamatController.text,
+        kota: HiveList(await repository.getBoxKota())..addAll(_selectedKota),
+      );
+
+      if (widget.dataSiswa != null) {
+        await repository.updateDataSiswa(siswa);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Siswa berhasil diubah'),
+          ),
+        );
+      } else {
+        await repository.addDataSiswa(siswa);
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Siswa berhasil ditambahkan'),
+          ),
+        );
+      }
+
+      Navigator.of(context).pop(true);
+    }
+  }
+
+  Future<void> _selectTanggalLahir(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null && picked != DateTime.now()) {
+      setState(() {
+        // Format the date to dd-MM-yyyy
+        _tanggalLahirController.text = DateFormat('dd-MM-yyyy').format(picked);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Data Siswa',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-        ),
+      appBar: const CustomAppBar(
+        title: 'Tambah Data Siswa',
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      floatingActionButton: FloatingActionButton(
-        foregroundColor: Colors.white,
-        shape: const CircleBorder(),
-        backgroundColor: const Color(0xffd22f3c),
-        onPressed: () {},
-        child: const Icon(
-          Icons.add_rounded,
-          size: 30,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  controller: _nisController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'NIS',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Silahkan isi NIS terlebih dahulu!';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: _namaController,
+                  decoration: const InputDecoration(
+                    labelText: 'Nama',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Silahkan isi Nama terlebih dahulu!';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () => _selectTanggalLahir(context),
+                  child: AbsorbPointer(
+                    child: TextFormField(
+                      controller: _tanggalLahirController,
+                      decoration: const InputDecoration(
+                        labelText: 'Tanggal Lahir',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Silahkan isi Tanggal Lahir terlebih dahulu!';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16.0),
+                const Text('Jenis Kelamin'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Radio<String>(
+                      value: 'Laki-laki',
+                      groupValue: jenkel,
+                      onChanged: (String? value) {
+                        setState(() {
+                          jenkel = value;
+                        });
+                      },
+                    ),
+                    const Text('Laki-Laki'),
+                    Radio<String>(
+                      value: 'Perempuan',
+                      groupValue: jenkel,
+                      onChanged: (String? value) {
+                        setState(() {
+                          jenkel = value;
+                        });
+                      },
+                    ),
+                    const Text('Perempuan'),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                TextFormField(
+                  controller: _alamatController,
+                  decoration: const InputDecoration(
+                    labelText: 'Alamat',
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Silahkan isi Alamat terlebih dahulu!';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 15),
+                FutureBuilder<List<DataKota>>(
+                  future: repository.getDataKota(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const CircularProgressIndicator();
+                    }
+                    final kotaList = snapshot.data!;
+                    return DropdownButtonFormField<DataKota>(
+                      value:
+                          _selectedKota.isNotEmpty ? _selectedKota.first : null,
+                      items: kotaList.map((kota) {
+                        return DropdownMenuItem<DataKota>(
+                          value: kota,
+                          child: Text(kota.nama),
+                        );
+                      }).toList(),
+                      onChanged: (kota) {
+                        setState(() {
+                          _selectedKota = kota != null ? [kota] : [];
+                        });
+                      },
+                      decoration: const InputDecoration(
+                        labelText: 'Kota',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null) {
+                          return 'Silahkan pilih Kota terlebih dahulu!';
+                        }
+                        return null;
+                      },
+                    );
+                  },
+                ),
+                const SizedBox(height: 20),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: _saveSiswa,
+                    child: const Text('Simpan'),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
-      body: const Center(
-        child: Text('Siswa Page'),
       ),
     );
   }
--- lib/pages/Siswa/siswaPage.dart
+++ lib/pages/Siswa/siswaPage.dart
@@ -1,35 +1,181 @@
+import 'package:data_siswa/model/dataSiswa.dart';
+import 'package:data_siswa/pages/Siswa/siswaAddPage.dart';
+import 'package:data_siswa/repository/dataSiswa.dart';
+import 'package:data_siswa/widgets/appbar.dart';
 import 'package:flutter/material.dart';
 
 class SiswaPage extends StatefulWidget {
-  const SiswaPage({super.key});
+  final DataSiswa? dataSiswa;
+  const SiswaPage({super.key, this.dataSiswa});
 
   @override
   State<SiswaPage> createState() => _SiswaPageState();
 }
 
 class _SiswaPageState extends State<SiswaPage> {
+  final DataSiswaRepository _repository = DataSiswaRepository();
+  List<DataSiswa> _dataSiswa = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSiswaList();
+  }
+
+  Future<void> _loadSiswaList() async {
+    // Open the box for datakotas first
+    await _repository.getBoxKota();
+    final boxSiswa = await _repository.getBoxSiswa();
+    setState(() {
+      _dataSiswa = boxSiswa.values.toList();
+    });
+
+    for (var siswa in _dataSiswa) {
+      print(
+          'Loaded Siswa: ${siswa.nama}, Kota: ${siswa.kota.map((k) => k.nama).join(', ')}');
+    }
+  }
+
+  Future<void> _deleteSiswa(DataSiswa siswa) async {
+    final bool? shouldDelete = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Delete'),
+          content: const Text('Are you sure you want to delete this siswa?'),
+          actions: [
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop(false); // Cancel delete
+              },
+            ),
+            TextButton(
+              child: const Text('Delete'),
+              onPressed: () {
+                Navigator.of(context).pop(true); // Confirm delete
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    if (shouldDelete == true) {
+      await _repository.deleteDataSiswa(siswa.id); // Perform deletion
+      _loadSiswaList();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Data berhasil dihapus.')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Data Siswa',
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-        ),
+      appBar: const CustomAppBar(
+        title: 'Data Siswa',
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
         foregroundColor: Colors.white,
         shape: const CircleBorder(),
         backgroundColor: const Color(0xffd22f3c),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.of(context)
+              .push(
+            MaterialPageRoute(
+              builder: (context) => SiswaAddPage(),
+            ),
+          )
+              .then((value) {
+            if (value != null) {
+              _loadSiswaList();
+            }
+          });
+        },
         child: const Icon(
           Icons.add_rounded,
           size: 30,
         ),
       ),
-      body: const Center(
-        child: Text('Siswa Page'),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: _dataSiswa.length,
+                itemBuilder: (context, index) {
+                  final dataSiswa = _dataSiswa[index];
+                  final dataKota =
+                      dataSiswa.kota.map((kota) => kota.nama).join(', ');
+                  return Card(
+                    elevation: 4,
+                    margin: const EdgeInsets.symmetric(vertical: 10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: ListTile(
+                      title: Text(dataSiswa.nama,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          )),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('NIS: ${dataSiswa.nis}'),
+                          Text('Tanggal Lahir: ${dataSiswa.tanggal_lahir}'),
+                          Text('Jenis Kelamin: ${dataSiswa.jenkel}'),
+                          Text('Alamat: ${dataSiswa.alamat}'),
+                          Text('Kota: $dataKota'),
+                        ],
+                      ),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          // Edit Button
+                          IconButton(
+                            icon: const Icon(
+                              Icons.edit,
+                              color: Colors.orange,
+                            ),
+                            onPressed: () {
+                              // Navigate to edit page
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => SiswaAddPage(
+                                      dataSiswa:
+                                          dataSiswa), // Pass the siswa to the edit page
+                                ),
+                              ).then((_) {
+                                // Reload siswa list after returning from edit page
+                                _loadSiswaList();
+                              });
+                            },
+                          ),
+                          // Delete Button
+                          IconButton(
+                            icon: const Icon(
+                              Icons.delete,
+                              color: Colors.red,
+                            ),
+                            onPressed: () {
+                              _deleteSiswa(dataSiswa);
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
