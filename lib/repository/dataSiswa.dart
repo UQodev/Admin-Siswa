6ae2d6292580a836d4c833f6dc72803588a0e295
Bajindul <ukisaw296@gmail.com>
2024-11-10 04:13:11 +0700

    menambah tabel siswa dan kota

--- lib/repository/dataSiswa.dart
+++ lib/repository/dataSiswa.dart
@@ -5,19 +5,19 @@ class DataSiswaRepository {
   static const String _boxNameSiswa = 'datasiswas';
   static const String _boxNameKota = 'datakotas';
 
-  Future<Box<DataSiswa>> _getBoxSiswa() async {
+  Future<Box<DataSiswa>> getBoxSiswa() async {
     return await Hive.openBox<DataSiswa>(_boxNameSiswa);
   }
 
-  Future<Box<DataKota>> _getBoxKota() async {
+  Future<Box<DataKota>> getBoxKota() async {
     return await Hive.openBox<DataKota>(_boxNameKota);
   }
 
   /* Siswa Repository */
   // Create
   Future<void> addDataSiswa(DataSiswa dataSiswa) async {
-    var boxSiswa = await _getBoxSiswa();
-    var boxKota = await _getBoxKota();
+    var boxSiswa = await getBoxSiswa();
+    var boxKota = await getBoxKota();
 
     dataSiswa.kota = HiveList(boxKota);
     dataSiswa.kota.addAll(dataSiswa.kota);
@@ -27,44 +27,44 @@ class DataSiswaRepository {
 
   // Get
   Future<DataSiswa?> getDataSiswa(String id) async {
-    var boxSiswa = await _getBoxSiswa();
+    var boxSiswa = await getBoxSiswa();
     return boxSiswa.get(id);
   }
 
   // Put / Update
   Future<void> updateDataSiswa(DataSiswa dataSiswa) async {
-    var boxSiswa = await _getBoxSiswa();
+    var boxSiswa = await getBoxSiswa();
     await boxSiswa.put(dataSiswa.id, dataSiswa);
   }
 
   // Delete
   Future<void> deleteDataSiswa(String id) async {
-    var boxSiswa = await _getBoxSiswa();
+    var boxSiswa = await getBoxSiswa();
     await boxSiswa.delete(id);
   }
 
   /* Kota Repository */
   // Create
   Future<void> addDataKota(DataKota dataKota) async {
-    var boxKota = await _getBoxKota();
+    var boxKota = await getBoxKota();
     await boxKota.put(dataKota.id, dataKota);
   }
 
   // Get / Read
-  Future<DataKota?> getDataKota(String id) async {
-    var boxKota = await _getBoxKota();
-    return boxKota.get(id);
+  Future<List<DataKota>> getDataKota() async {
+    var boxKota = await getBoxKota();
+    return boxKota.values.toList();
   }
 
   // Put / Update
   Future<void> updateDataKota(DataKota dataKota) async {
-    var boxKota = await _getBoxKota();
+    var boxKota = await getBoxKota();
     await boxKota.put(dataKota.id, dataKota);
   }
 
   // Delete
   Future<void> deleteDataKota(String id) async {
-    var boxKota = await _getBoxKota();
+    var boxKota = await getBoxKota();
     await boxKota.delete(id);
   }
 }
